3a5c8f925fd36dd3000a1bac2571113c22dcb7e6
Alp Akpinar <aakpinar@bu.edu>
2021-07-09 08:02:01 -0500

    Update input

--- makeWorkspace/process_vbf.sh
+++ makeWorkspace/process_vbf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 TAG='latest'
-INDIR=../input/vbf/merged_2021-07-01_vbfhinv_ULv8_05Feb21
+INDIR=../input/vbf/merged_2021-07-08_vbfhinv_ULv8 
 INDIR="$(readlink -e $INDIR)"
 
 OUTDIR="../vbf/$(basename $INDIR)/${TAG}/root"
