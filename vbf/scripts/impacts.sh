e6f326f2d69a671a2da36499f920ff55aab96d6f
Alp Akpinar <aakpinar@bu.edu>
2021-07-09 14:29:06 -0500

    Compute Asimov impacts

--- vbf/scripts/impacts.sh
+++ vbf/scripts/impacts.sh
@@ -6,9 +6,9 @@ pushd impacts_nocondor
 for YEAR in 2018 combined; do
     mkdir ${YEAR}
     pushd ${YEAR}
-    combineTool.py -M Impacts -d ../../cards/card_vbf_${YEAR}.root -m 125 --doInitialFit --robustFit 1 --parallel=4 --rMin=-1
-    combineTool.py -M Impacts -d ../../cards/card_vbf_${YEAR}.root -m 125 --robustFit 1 --doFits --parallel 4 --parallel=4 --rMin=-1
-    combineTool.py -M Impacts -d ../../cards/card_vbf_${YEAR}.root -m 125 -o impacts.json  --parallel=4 --rMin=-1
+    combineTool.py -M Impacts -t -1 -d ../../cards/card_vbf_${YEAR}.root -m 125 --doInitialFit --robustFit 1 --parallel=4 --rMin=-1
+    combineTool.py -M Impacts -t -1 -d ../../cards/card_vbf_${YEAR}.root -m 125 --robustFit 1 --doFits --parallel 4 --parallel=4 --rMin=-1
+    combineTool.py -M Impacts -t -1 -d ../../cards/card_vbf_${YEAR}.root -m 125 -o impacts.json  --parallel=4 --rMin=-1
     popd
     plotImpacts.py -i ${YEAR}/impacts.json -o impacts_${YEAR} --blind
 
