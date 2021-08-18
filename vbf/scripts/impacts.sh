4a8672683f48549c942ff6007583cbb8dfec898e
Alp Akpinar <aakpinar@bu.edu>
2021-08-18 10:23:46 -0500

    Limits and impacts: No Asimov

--- vbf/scripts/impacts.sh
+++ vbf/scripts/impacts.sh
@@ -11,9 +11,9 @@ cat ${FILEPATH} > "impactScript.sh"
 for YEAR in 2017 2018; do
     mkdir ${YEAR}
     pushd ${YEAR}
-    combineTool.py -M Impacts -t -1 -d ../../cards/card_vbf_${YEAR}.root -m 125 --doInitialFit --robustFit 1 --parallel=4 --rMin=-1
-    combineTool.py -M Impacts -t -1 -d ../../cards/card_vbf_${YEAR}.root -m 125 --robustFit 1 --doFits --parallel 4 --parallel=4 --rMin=-1
-    combineTool.py -M Impacts -t -1 -d ../../cards/card_vbf_${YEAR}.root -m 125 -o impacts.json  --parallel=4 --rMin=-1
+    combineTool.py -M Impacts -d ../../cards/card_vbf_${YEAR}.root -m 125 --doInitialFit --robustFit 1 --parallel=4 --rMin=-1
+    combineTool.py -M Impacts -d ../../cards/card_vbf_${YEAR}.root -m 125 --robustFit 1 --doFits --parallel 4 --parallel=4 --rMin=-1
+    combineTool.py -M Impacts -d ../../cards/card_vbf_${YEAR}.root -m 125 -o impacts.json  --parallel=4 --rMin=-1
     popd
     plotImpacts.py -i ${YEAR}/impacts.json -o impacts_${YEAR} --blind
 
