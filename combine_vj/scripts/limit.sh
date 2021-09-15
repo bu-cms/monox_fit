ec93d88ce030a16bb592b947e88c19ebfcd0aa34
Andreas Albert <andreas.albert@cern.ch>
2021-09-15 14:02:25 +0200

    Update combined limit script

--- combine_vj/scripts/limit.sh
+++ combine_vj/scripts/limit.sh
@@ -1,26 +1,25 @@
 
-mkdir -p limit5
-pushd limit5
+mkdir -p limit
+pushd limit
 
 #remove limit on stack size to prevent related segfault
 ulimit -s unlimited
 
 for YEAR in combined; do
-    # nohup combine -M AsymptoticLimits \
-    #               -t -1 \
-    #               -n monojet_monov_nominal_asimov_${YEAR} \
-    #               ../cards/card_monojet_monov_nominal_${YEAR}.root \
-    #               --setParameters LUMISCALE=1 \
-    #               --freezeParameters LUMISCALE \
-    #               --cminDefaultMinimizerStrategy 0 \
-    #               &> log_asimov_${YEAR}.txt &
+    nohup combine -M AsymptoticLimits \
+                   -t -1 \
+                   -n monojet_monov_nominal_asimov_${YEAR} \
+                   ../cards/card_monojet_monov_nominal_${YEAR}.root \
+                   --setParameters LUMISCALE=1 \
+                   --freezeParameters LUMISCALE \
+                   --cminDefaultMinimizerStrategy 0 \
+                   &> log_asimov_${YEAR}.txt &
     nohup combine -M AsymptoticLimits \
                   -n monojet_monov_nominal_${YEAR} \
                   ../cards/card_monojet_monov_nominal_${YEAR}.root \
                   --setParameters LUMISCALE=1 \
                   --freezeParameters LUMISCALE \
                   --cminDefaultMinimizerStrategy 0 \
-                  --trackParameters 'rgx{.*}' \
                   &> log_${YEAR}.txt &
 done
 popd
